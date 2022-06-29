6de3e9089aa9f3afbc0a0fd5d101912f14b2adfa
Ron Earward <nipponman32@protonmail.com>
2022-06-29 19:50:22 -0400

    Removed tests

--- Solution.java
+++ Solution.java
@@ -1,10 +1,8 @@
 import java.lang.Math;
-import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
 
 public class Solution {
-  public static int zeros(int n) {
+
+public static int zeros(int n) {
     int z=0;
     for(int k=1;k<log5(n);k++){
       z += n/Math.pow(5,k);
@@ -15,12 +13,5 @@ public class Solution {
   
   public static double log5(int t){
     return Math.log((double)t)/Math.log(5.0);
-  }
-  
-  @Test
-  public static void main(String[] args){
-      assertThat(Solution.zeros(0), is(0)); 
-    assertThat(Solution.zeros(6), is(1)); 
-    assertThat(Solution.zeros(14), is(2));    
   }
 }
